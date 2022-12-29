bf9a94eab6af5a23e16406f94e0762e4be19cfa4
Witold Walis <witoldwalis@gmail.com>
2022-12-29 15:45:09 +0100

    function added

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
-let currencyElement = document.querySelector(".js-whatCurrency");
+function calculate(){
+const formElement = document.querySelector(".js-form");
+const amountElement = document.querySelector(".js-amount");
+const resultElement = document.querySelector(".js-result");
+const currencyElement = document.querySelector(".js-whatCurrency");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
     let amount = amountElement.value;
     let currency = currencyElement.value;
-
     let result = (amount / currency);
-
     resultElement.innerText = result.toFixed(2);
 
 });
-
+}
 
